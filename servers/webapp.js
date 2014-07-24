e6fbf6a557df6ef4e665df069d53cd87445f26bd
Chris Fricke <chris.fricke@gmail.com>
2014-07-24 03:30:37 -0700

    fix q bug

--- servers/webapp.js
+++ servers/webapp.js
@@ -27,6 +27,7 @@ module.exports = function(app, router, mongoose) {
   app.use(bodyParser.urlencoded({
     extended: true
   }));
+  
   app.use(bodyParser.json());
   app.use(methodOverride());
   app.use(cookieParser());
